03b518891e2db86254b084a17283a7d74ab7cf28
sayan-academic <sayanacademic69@gmail.com>
2026-05-05 16:14:33 +0530

    added allowed hosts and debug=False

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -28,10 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'generic-chatbot-3433.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
